7d5299089291612d873fd6a0f4acb12bb890c980
Bruno Fraccaroli <brunovfraccaroli@gmail.com>
2025-06-09 17:58:52 -0300

    adicinei a função parse_page para receber o nome e valor do produto

--- app_01.py
+++ app_01.py
@@ -1,5 +1,12 @@
 import requests
 
-url = "https://www.mercadolivre.com.br/apple-iphone-16-pro-1-tb-titnio-preto-distribuidor-autorizado/p/MLB1040287851#polycard_client=search-nordic&wid=MLB5054621110&sid=search&searchVariation=MLB1040287851&position=2&search_layout=stack&type=product&tracking_id=a2201312-8133-4041-83eb-9ef4bec42f10"
-response = requests.get(url)
-print(response.text)
+#Fiz uma função para pegar a URL do mercado livre com o produto.
+
+def fetch_page():
+    response = requests.get(url)
+    return response.text
+
+if __name__ == "__main__":
+    url = "https://www.mercadolivre.com.br/apple-iphone-16-pro-1-tb-titnio-preto-distribuidor-autorizado/p/MLB1040287851#polycard_client=search-nordic&wid=MLB5054621110&sid=search&searchVariation=MLB1040287851&position=2&search_layout=stack&type=product&tracking_id=a2201312-8133-4041-83eb-9ef4bec42f10"
+    page_content = fetch_page()
+    print(page_content)
